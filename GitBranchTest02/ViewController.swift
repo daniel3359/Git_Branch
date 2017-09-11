9e60d3142304079cc1b7219c242373df9a964fb3
D7702_09 <d7702_09@d7702-09ui-imac.local>
2017-09-11 15:18:26 +0900

    Add develop branch

--- GitBranchTest02/ViewController.swift
+++ GitBranchTest02/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Git Testddd")
+        print("Switch to develop")
     }
 
     
